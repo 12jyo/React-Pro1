97baa4462213877aa861093f64ea5df9f6983068
jyo12 <jyotiss1312@gmail.com>
2021-09-20 18:57:32 +0530

    first commit react project

---
+++ src/App.js
@@ -0,0 +1,12 @@
+
+import React from 'react'
+import Restuarant from './components/basics/Restuarant'
+// import UseState from './components/Hooks/useState';
+// import UseEffect from './components/Hooks/useEffect';
+// import UseReducer from './components/Hooks/useReducer'
+
+const App = () => {
+  return  <Restuarant/>
+}
+
+export default App
